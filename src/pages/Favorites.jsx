757b83a0dc36cb153cdc1c56bc0d7aeba885e69b
sueherrera30 <sueherrera30@gmail.com>
2022-06-08 17:00:25 -0500

    feat: animations and responsive

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { MdOutlineArrowBack as Back } from "react-icons/md";
 
+import hearts from '../assets/favs.png';
 import thinking from '../assets/thinking.png'
 import sharedStyles from './styles/home.module.css';
 import styles from './styles/favorites.module.css';
@@ -14,6 +15,7 @@ const Favorites = () => {
                     <Back  className={styles.icons} onClick={() => navigate('/')} />
                     <div className={styles.imageContainer} >
                         <img alt="decoration" src={thinking} className={styles.image} />
+                        <img src={hearts} alt="hearts" className={styles.hearts}/>
                     </div>
                     <h2 className={styles.title}>MY FAVORITES WORDS</h2>
                 </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,9 @@ import Input from '../components/Input';
 import SavedWords from '../components/SavedWords';
 import styles from './styles/home.module.css';
 import main from '../assets/main.png'
+import school from '../assets/school.png';
+import microscopy from '../assets/microscopy.png';
+import world from '../assets/world.png';
 
 const Home = () => {
     const [word, setWord] = useState();
@@ -13,6 +16,9 @@ const Home = () => {
             <div className={styles.container}>
                 <div className={styles.imageContainer}>
                     <img src={main} alt="main" className={styles.image}/>
+                    <img src={school} alt="main" className={`${styles.floatingImage} ${styles.school}`}/>
+                    <img src={microscopy} alt="main" className={`${styles.floatingImage} ${styles.microscopy}`}/>
+                    <img src={world} alt="main" className={`${styles.floatingImage} ${styles.world}`}/>
                 </div>
                 <h1 className={styles.title}>MY DICTIONARY</h1>  
                 <p className={styles.subtitle}>Find meanings and save for quick reference</p>
