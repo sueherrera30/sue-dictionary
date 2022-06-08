16e76e745a2883c23770e4ba1124ad09d37b0131
sueherrera30 <sueherrera30@gmail.com>
2022-06-08 15:56:09 -0500

    feat: endfavorites view

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { MdOutlineArrowBack as Back } from "react-icons/md";
 
-// import styles from './styles/home.module.css';
-
+import thinking from '../assets/thinking.png'
+import sharedStyles from './styles/home.module.css';
+import styles from './styles/favorites.module.css';
 const Favorites = () => {
-    console.log("hola favorites")
+    let navigate = useNavigate();
     return (
-        <section>
-         favorites!!!!
+        <section className={sharedStyles.mainContainer}>
+            <div className={`${sharedStyles.container} ${styles.container}`}>
+                <header className={styles.header}>
+                    <Back  className={styles.icons} onClick={() => navigate('/')} />
+                    <div className={styles.imageContainer} >
+                        <img alt="decoration" src={thinking} className={styles.image} />
+                    </div>
+                    <h2 className={styles.title}>MY FAVORITES WORDS</h2>
+                </header>
+                <div className={styles.wordsContainer} >
+                    <div className={styles.word} >fvoritos</div>
+                </div>
+            </div>
         </section>
     )
 };
